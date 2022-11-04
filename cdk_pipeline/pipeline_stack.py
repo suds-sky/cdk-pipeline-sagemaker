82749d3b8509ce1dae75c2d08d962b721538ce58
suds-sky <sudhakar.kirubakaran@sky.uk>
2022-11-04 14:42:11 +0000

    test

--- cdk_pipeline/pipeline_stack.py
+++ cdk_pipeline/pipeline_stack.py
@@ -26,7 +26,7 @@ class PipelineStack(Stack):
                 "npm install -g aws-cdk",
                 "curl -sSL https://install.python-poetry.org | python3 -",
                 "export PATH=\"/root/.local/bin:$PATH\"",
-                "poetry config settings.virtualenvs.in-project true",
+                "poetry config virtualenvs.create false",
                 "poetry install --no-root",
                 "cdk synth --no-lookups"
             ],
